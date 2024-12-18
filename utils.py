e3de433f3373a679667d6b4b54e3102d3f307257
DinoFlix <dinoflix44@gmail.com>
2024-12-18 14:13:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -114,4 +114,193 @@ async def get_poster(query, bulk=False, id=False, file=None):
             "languages": list_to_str(movie.get("languages")),
             "director": list_to_str(movie.get("director")),
             "writer": list_to_str(movie.get("writer")),}
-            
+            "producer":list_to_str(movie.get("producer")),
+            "composer":list_to_str(movie.get("composer")) ,
+            "cinematographer":list_to_str(movie.get("cinematographer")),
+            "music_team": list_to_str(movie.get("music department")),
+            "distributors": list_to_str(movie.get("distributors")),
+            'release_date': date,
+            'year': movie.get('year'),
+            'genres': list_to_str(movie.get("genres")),
+            'poster': movie.get('full-size cover url' , START_IMG),
+            'plot': plot,
+            'rating': str(movie.get("rating")),
+            'url':f'https://www.imdb.com/title/tt{movieid}'
+            }
+
+            async def users_broadcast(user_id, message, is_pin):
+            try:
+          m=await message.copy(chat_id=user_id)
+         if is_pin:
+            await m.pin(both_sides=True)
+        return True, "Success"
+        except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await users_broadcast(user_id, message)
+       except InputUserDeactivated:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        await db.delete_user(user_id)
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
+    except Exception as e:
+        return False, "Error"
+
+async def groups_broadcast(chat_id, message, is_pin):
+    try:
+        m = await message.copy(chat_id=chat_id)
+        if is_pin:
+            try:
+                await m.pin()
+            except:
+                pass
+        return "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await groups_broadcast(chat_id, message)
+    except Exception as e:
+        await db.delete_chat(chat_id)
+        return "Error"
+
+async def get_settings(group_id , pm_mode = False):
+    if pm_mode:
+        return SETTINGS.copy()
+    else:
+        settings = await db.get_settings(group_id)
+    return settings 
+    
+async def save_group_settings(group_id, key, value):
+    current = await get_settings(group_id)
+    current.update({key: value})
+    temp.SETTINGS.update({group_id: current})
+    await db.update_settings(group_id, current)
+
+def get_size(size):
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
+def get_name(name):
+    regex = re.sub(r'@\w+', '', name)
+    return regex
+
+def list_to_str(k):    
+    if not k:
+        return "N/A"
+    elif len(k) == 1:
+        return str(k[0])
+    else:
+        return ', '.join(str(item) for item in k)
+
+
+async def get_shortlink(link, grp_id, is_second_shortener=False, is_third_shortener=False , pm_mode=False):
+    if not pm_mode:
+        settings = await get_settings(grp_id)
+    else:
+        settings = SETTINGS
+    if IS_VERIFY:
+        if is_third_shortener:             
+            api, site = settings['api_three'], settings['shortner_three']
+        else:
+            if is_second_shortener:
+                api, site = settings['api_two'], settings['shortner_two']
+            else:
+                api, site = settings['api'], settings['shortner']
+        shortzy = Shortzy(api, site)
+        try:
+            link = await shortzy.convert(link)
+        except Exception as e:
+            link = await shortzy.get_quick_link(link)
+    return link
+
+def get_file_id(message: "Message") -> Any:
+    media_types = (
+        "audio",
+        "document",
+        "photo",
+        "sticker",
+        "animation",
+        "video",
+        "voice",
+        "video_note",
+    )    
+    if message.media:
+        for attr in media_types:
+            media = getattr(message, attr, None)
+            if media:
+                setattr(media, "message_type", attr)
+                return media
+
+#def get_hash(media_msg: Message) -> str:
+#    media = get_file_id(media_msg)
+ #   return getattr(media, "file_unique_id", "")[:6]
+
+def get_status():
+    tz = pytz.timezone('Asia/Kolkata')
+    hour = datetime.now(tz).time().hour
+    if 5 <= hour < 12:
+        sts = "𝐺𝑜𝑜𝑑 𝑀𝑜𝑟𝑛𝑖𝑛𝑔"
+    elif 12 <= hour < 18:
+        sts = "𝐺𝑜𝑜𝑑 𝐴𝑓𝑡𝑒𝑟𝑛𝑜𝑜𝑛"
+    else:
+        sts = "𝐺𝑜𝑜𝑑 𝐸𝑣𝑒𝑛𝑖𝑛𝑔"
+    return sts
+
+async def is_check_admin(bot, chat_id, user_id):
+    try:
+        member = await bot.get_chat_member(chat_id, user_id)
+        return member.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER]
+    except:
+        return False
+
+async def get_seconds(time_string):
+    def extract_value_and_unit(ts):
+        value = ""
+        unit = ""
+        index = 0
+        while index < len(ts) and ts[index].isdigit():
+            value += ts[index]
+            index += 1
+        unit = ts[index:].lstrip()
+        if value:
+            value = int(value)
+        return value, unit
+    value, unit = extract_value_and_unit(time_string)
+    if unit == 's':
+        return value
+    elif unit == 'min':
+        return value * 60
+    elif unit == 'hour':
+        return value * 3600
+    elif unit == 'day':
+        return value * 86400
+    elif unit == 'month':
+        return value * 86400 * 30
+    elif unit == 'year':
+        return value * 86400 * 365
+    else:
+        return 0
+
+def get_readable_time(seconds):
+    periods = [('days', 86400), ('hour', 3600), ('min', 60), ('sec', 1)]
+    result = ''
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            result += f'{int(period_value)}{period_name}'
+    return result
+
+async def save_default_settings(id):
+    await db.reset_group_settings(id)
+    current = await db.get_settings(id)
+    temp.SETTINGS.update({id: current})
